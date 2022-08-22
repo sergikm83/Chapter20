ebb6c440a96c39b67c00cdf3f642197e367f0336
Sergey Misko <sergik.m@outlook.com>
2022-08-22 22:30:20 +0500

    добавил вывод информации обо всех (запоминающих) устройствах

--- DriveInfoApp/Program.cs
+++ DriveInfoApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace DriveInfoApp
 {
@@ -6,7 +7,23 @@ namespace DriveInfoApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("***** Fun with DriveInfo *****\n");
+            // Получить информацию обо всех устройствах.
+            DriveInfo[] myDrives = DriveInfo.GetDrives();
+            // Вывести сведения об устройствах.
+            foreach (DriveInfo drive in myDrives)
+            {
+                Console.WriteLine("Name: {0}", drive.Name);
+                Console.WriteLine("Type: {0}", drive.DriveType);
+                // Проверить смонтировано ли устройство.
+                if (drive.IsReady)
+                {
+                    Console.WriteLine("Free space: {0}", drive.TotalFreeSpace);
+                    Console.WriteLine("Format: {0}", drive.DriveFormat);
+                    Console.WriteLine("Label: {0}", drive.VolumeLabel);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
